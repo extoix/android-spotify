b54c1561ac765e15b271c674a3cc67a4e013159f
John Kuk <extoix@users.noreply.github.com>
2015-07-27 00:13:26 -0500

    Initial creation of AsyncTaks SearchTask to perform Spotify lookup of artist.

--- Spotify/app/src/main/java/com/extoix/android/spotify/MainActivityFragment.java
+++ Spotify/app/src/main/java/com/extoix/android/spotify/MainActivityFragment.java
@@ -1,10 +1,19 @@
 package com.extoix.android.spotify;
 
-import android.support.v4.app.Fragment;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import java.util.List;
+
+import kaaes.spotify.webapi.android.SpotifyApi;
+import kaaes.spotify.webapi.android.SpotifyService;
+import kaaes.spotify.webapi.android.models.Artist;
+import kaaes.spotify.webapi.android.models.ArtistsPager;
+import kaaes.spotify.webapi.android.models.Image;
 
 public class MainActivityFragment extends Fragment {
 
@@ -13,8 +22,37 @@ public class MainActivityFragment extends Fragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        View rootView = inflater.inflate(R.layout.fragment_main, container, false);
 
+        SearchTask searchTask = new SearchTask();
+        searchTask.execute();
+
+        View rootView = inflater.inflate(R.layout.fragment_main, container, false);
         return rootView;
     }
+
+    public class SearchTask extends AsyncTask<Void, Void, Void> {
+
+        @Override
+        protected Void doInBackground(Void... strings) {
+
+            SpotifyApi spotifyApi = new SpotifyApi();
+            SpotifyService spotifyService = spotifyApi.getService();
+
+            ArtistsPager artistResults = spotifyService.searchArtists("Cake");
+
+            List<Artist> artistList = artistResults.artists.items;
+            for(Artist artist: artistList) {
+                String artistName = artist.name;
+
+                List<Image> imagesList = artist.images;
+                int lastImageIndex = imagesList.size() - 1;
+                String imageURL = imagesList.get(lastImageIndex).url;
+
+                String something = "";
+            }
+
+            return null;
+        }
+    }
+
 }
